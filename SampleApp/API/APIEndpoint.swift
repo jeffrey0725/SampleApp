e162494e2155a7880e31b4c4c65af53905619e3b
Jeffrey Cheung <jeffreycheung1995@gmail.com>
2022-06-22 14:47:08 -0400

    - Added "APIEndpoint" and "APIPath"

---
+++ SampleApp/API/APIEndpoint.swift
@@ -0,0 +1,12 @@
+//
+//  APIEndpoint.swift
+//  SampleApp
+//
+//  Created by Jeffrey Cheung on 22/6/2022.
+//
+
+import Foundation
+
+struct APIEndpoint {
+    var baseEndpoint = "https://api.disneyapi.dev"
+}
---
+++ SampleApp/API/APIPath.swift
@@ -0,0 +1,12 @@
+//
+//  APIPath.swift
+//  SampleApp
+//
+//  Created by Jeffrey Cheung on 22/6/2022.
+//
+
+import Foundation
+
+struct APIPath {
+    var getCharacter = "/characters"
+}
